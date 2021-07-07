55c5d1aa999675f81bef09a8d071a067f5a89969
Pringle <80323633+PringlePot@users.noreply.github.com>
2021-07-07 17:31:43 +0200

    add menu system for typescript bot

--- src/events/button/ButtonEvent.ts
+++ src/events/button/ButtonEvent.ts
@@ -1,6 +1,7 @@
 import BaseEvent from '../../utils/structures/BaseEvent';
 import DiscordClient from '../../client/client';
 import {ButtonInteraction, Interaction} from 'discord.js';
+import {menus} from '../../utils/ButtonMenu';
 export default class ButtonEvent extends BaseEvent {
   constructor() {
     super('interactionCreate');
@@ -8,13 +9,12 @@ export default class ButtonEvent extends BaseEvent {
 
   async run(client: DiscordClient, button: Interaction) {
     if (button instanceof ButtonInteraction) {
-      if (button.customId === 'happy_cat') {
-        await button.reply({content: '😺', ephemeral: true});
-      } else if (button.id === 'mad_cat') {
-        await button.reply({
-          content: '<:ragecat:853330321277714523>',
-          ephemeral: true,
-        });
+      const menu = menus.find(value => value.messageId === button.message.id);
+      if (menu) {
+        if (button.user.id !== menu.creator && menu.creatorOnly) {
+          return;
+        }
+        menu.onClick(button);
       }
     }
   }
